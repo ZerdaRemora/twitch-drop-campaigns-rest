cce69054744839b38a58923d76505c8b5a822185
ZerdaRemora <james@zerdaremora.tech>
2021-04-02 19:14:22 +0100

    Initial commit.

---
+++ src/main/kotlin/gy/zr/twitchdropcampaignsrest/DropCampaignController.kt
@@ -0,0 +1,56 @@
+package gy.zr.twitchdropcampaignsrest
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "games")
+data class Game(
+    @Id val id: Int,
+    val name: String,
+)
+
+@Entity
+@Table(name = "drops")
+data class DropCampaign(
+    @Id val id: String,
+    val name: String,
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    val game: Game,
+    val status: String,
+    val started: Date,
+    val ended: Date
+)
+
+interface DropCampaignRepository : CrudRepository<DropCampaign, String> {
+
+    fun findAllByStatusIs(status: String): List<DropCampaign>
+}
+
+@RestController
+class DropCampaignController(val repository: DropCampaignRepository) {
+
+    @GetMapping("/all")
+    fun all(): List<DropCampaign> {
+        return repository.findAll().toList()
+    }
+
+    @GetMapping("/active")
+    fun activeCampaigns(): List<DropCampaign> {
+        return repository.findAllByStatusIs("ACTIVE")
+    }
+
+    @GetMapping("/upcoming")
+    fun upcomingCampaigns(): List<DropCampaign> {
+        return repository.findAllByStatusIs("UPCOMING")
+    }
+
+    @GetMapping("/expired")
+    fun expiredCampaigns(): List<DropCampaign> {
+        return repository.findAllByStatusIs("EXPIRED")
+    }
+}
---
+++ src/test/kotlin/gy/zr/twitchdropcampaignsrest/TwitchDropCampaignsRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package gy.zr.twitchdropcampaignsrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TwitchDropCampaignsRestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
