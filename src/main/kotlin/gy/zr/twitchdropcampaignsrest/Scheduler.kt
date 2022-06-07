ff69fcb74447b10c4abcb891d9cf25eb0c9dda1d
ZerdaRemora <james@zerdaremora.tech>
2022-06-07 22:09:17 +0100

    Fixed not grabbing system timezone correctly.

--- src/main/kotlin/gy/zr/twitchdropcampaignsrest/Scheduler.kt
+++ src/main/kotlin/gy/zr/twitchdropcampaignsrest/Scheduler.kt
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
 import java.util.*
 import javax.annotation.PostConstruct
 
@@ -29,14 +30,18 @@ class Scheduler(val repository: DropCampaignRepository) {
             cal.add(Calendar.MINUTE, -10)
 
             val tenMinsTz = LocalDateTime.ofInstant(cal.time.toInstant(), ZoneId.of("UTC"))
-            val nowTz = LocalDateTime.now()
+            val nowDefaultTz = LocalDateTime.now().atZone(ZoneId.systemDefault())
 
             val zonedTenMins = tenMinsTz.atZone(ZoneId.of("UTC"))
-            val zonedNow = nowTz.atZone(ZoneId.of("UTC"))
+            val zonedNow = nowDefaultTz.withZoneSameInstant(ZoneId.of("UTC"))
             val zonedStarted = LocalDateTime.ofInstant(it.started.toInstant(), ZoneId.of("UTC")).atZone(ZoneId.of("UTC"))
             val format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")
 
+//            println("nowDefaultTz: $nowDefaultTz, tenMins: $tenMinsTz, defaultZone: ${ZoneId.systemDefault().getDisplayName(TextStyle.SHORT, Locale.ENGLISH)}")
+//            println("zoned now: $zonedNow, zonedTenMins: $zonedTenMins (zonedStarted: $zonedStarted)")
+
             if (zonedNow.isAfter(zonedTenMins) && zonedNow.isBefore(zonedStarted)) { // Drop starts within next 10 mins so notify.
+//                println("[DEBUG] $zonedNow is after $zonedTenMins")
                 println("${it.name} at ${it.started}")
                 bot.sendMessage("${it.name} (${it.game.name}) starts at ${format2.format(zonedStarted.withZoneSameInstant(ZoneId.of("Europe/London")))} " +
                         "(${it.started} UTC)")
